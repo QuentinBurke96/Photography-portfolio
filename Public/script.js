2d59c6b7a9180ff8432d49b0d1730e33c798e4fb
quentinburke96 <qeburke.qe@gmail.com>
2023-07-19 16:53:45 -0400

    I DID IITTT... kinda

--- Public/script.js
+++ Public/script.js
@@ -1,30 +1,34 @@
 const showBtn = document.querySelector('.navBtn');
 const topNav = document.querySelector('.top-nav');
-const contactForm = document.querySelector('form')
+const contactForm = document.querySelector('form');
+let name = document.getElementById('name');
+let email = document.getElementById('email')
+let subject = document.getElementById('subject')
+let message = document.getElementById('message')
 
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault()
     
     let formData = {
-        name: nm.value,
-        email: mail.value,
+        name: name.value,
+        email: email.value,
         subject: subject.value,
         message: message.value
     }
 
-    let xhr = new XMLHttpRequest()
+    let xhr = new XMLHttpRequest();
 
-    xhr.open('POST', '/')
-    xhr.setRequestHeader('content-type', 'application/json')
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('content-type', 'application/json');
     xhr.onload = function() {
         console.log(xhr.responseText);
         if(xhr.responseText == 'success'){
             alert('Email Sent');
-            nm.value,
-            mail.value,
-            subject.value,
-            message.value
+            name.value = '',
+            email.value = '',
+            subject.value = '',
+            message.value = ''
         }else{
             alert('Something Wrong!')
         }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const nodemailer = require('nodemailer')
 
 const PORT = process.env.PORT || 3000
 
@@ -14,8 +15,33 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'qkspace.info@gmail.com',
+            pass: 
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'qkspace.info@gmail.com',
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email sent ' + info.response);
+            res.send('Success')
+        }
+     })
 })
 
 app.listen(PORT, () => {
-    console.log(`wiufhwirufh ${PORT}`)
+    console.log(`Speeding on port ${PORT}`)
 })
